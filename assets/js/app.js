dde8ed5f4b9171cbc5f74a71f0878be58bcaa012
Ashell Fernando <ashellz2010@gmail.com>
2019-03-27 13:27:53 -0400

    functions are now working and connected firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,107 +1,112 @@
 // Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyBc8V8kvpScxILBHm1bxkY2BdY3gLnw3mQ',
+  authDomain: 'trainswatch-81413.firebaseapp.com',
+  databaseURL: 'https://trainswatch-81413.firebaseio.com',
+  projectId: 'trainswatch-81413',
+  storageBucket: 'trainswatch-81413.appspot.com',
+  messagingSenderId: '997350962243'
+};
 
 firebase.initializeApp(config);
 
 var db = firebase.database();
 
 $('#add-train-btn').on('click', e => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // Grab user input
-    let eName = $('#employee-name-input')
-        .val()
-        .trim();
+  // Grab user input
+  let tName = $('#train-name-input')
+    .val()
+    .trim();
 
-    let eRole = $('#role-input')
-        .val()
-        .trim();
+  let tDes = $('#destination-input')
+    .val()
+    .trim();
 
-    let eStart = moment(
-        $('#start-input')
-            .val()
-            .trim(),
-        'MM/DD/YYYY'
-    ).format('L');
-    let eRate = $('#rate-input')
-        .val()
-        .trim();
+  let tTime = moment(
+    $('#train-first-input')
+      .val()
+      .trim(),
+    'MM/DD/YYYY'
+  ).format('L');
+  let tFreq = $('#train-frequency-input')
+    .val()
+    .trim();
 
-    // conditional checking if there are data in form fields.
-    // Don't want empty datasets
-    if (
-        eName.length === 0 ||
-        eRole.length === 0 ||
-        // eStart === 'Invalid date' ||
-        eRate.length === 0
-    ) {
-        $('#error')
-            .html('Please fill out ALL the form fields!')
-            .show();
-        setTimeout(() => {
-            $('#error').hide();
-        }, 3000);
-    } else {
-        //Create temp obj
+  // conditional checking if there are data in form fields.
+  // Don't want empty datasets
+  if (tName.length === 0 || tDes.length === 0 || tFreq.length === 0) {
+    $('#error')
+      .html('Please fill out ALL the form fields!')
+      .show();
+    setTimeout(() => {
+      $('#error').hide();
+    }, 3000);
+  } else {
+    //Create temp obj
 
-        let newEmp = {
-            name: eName,
-            role: eRole,
-            start: eStart,
-            rate: eRate
-        };
+    let newLine = {
+      name: tName,
+      destination: tDes,
+      startTime: tTime,
+      frequency: tFreq
+    };
 
-        //uploads temp obj to firebase db
-        db.ref().push(newEmp);
+    //uploads temp obj to firebase db
+    //!!!!!! IMPORTANT
+    db.ref().push(newLine);
 
-        //console logs
-        console.log(
-            `Entry for ${newEmp.name} with role of ${
-                newEmp.role
-            } with starting date on ${newEmp.start} and payrate of ${
-                newEmp.rate
-            } HAS BEEN ADDED!`
-        );
-        // Clears text values
-        $('#employee-name-input').val('');
-        $('#role-input').val('');
-        $('#start-input').val('');
-        $('#rate-input').val('');
-    }
+    //console logs
+    console.log(
+      `Entry for ${newLine.name} with destination of ${
+        newLine.destination
+      } and with start time ${newLine.startTime} with frequent of ${
+        newLine.frequency
+      } HAS BEEN ADDED!`
+    );
+    // Clears text values
+    $('#train-name-input').val('');
+    $('#destination-input').val('');
+    $('#train-first-input').val('');
+    $('#train-frequency-input').val('');
+  }
+  return false;
 });
 
-// 3. Create a way to retrieve employees from the employee database.
+// 3. Create a way to retrieve train info from the trainswatch database.
 
 //gets entries on the DB
 db.ref().on('child_added', childSnap => {
-    console.log(childSnap.val());
+  console.log(childSnap.val());
 
-    //declaring again for when a 'child is added'
-    let eName = childSnap.val().name,
-        eRole = childSnap.val().role,
-        eStart = childSnap.val().start,
-        eRate = childSnap.val().rate;
+  //declaring again for when a 'child is added'
+  let dtName = childSnap.val().name,
+    dtDestination = childSnap.val().destination,
+    dtTime = childSnap.val().startTime,
+    dtFreq = childSnap.val().frequency;
 
-    //logging the values
-    console.log(`${eName} ${eRole} ${eStart} ${eRate} `);
+  //logging the values
+  console.log(`${dtName} ${dtDestination} ${dtTime} ${dtFreq} `);
 
-    //using moment.js to calculate the start date from today, in months. returns a number
-    let workedMonths = moment().diff(moment(eStart, 'L'), 'months');
+  //using moment.js to calculate the start date from today, in months. returns a number
+  //   let workedMonths = moment().diff(moment(dtName, 'L'), 'months');
 
-    //using that returned number to multiply rate to find $$$$
-    let eBilled = (workedMonths * eRate).toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD'
-    });
+  //   //using that returned number to multiply rate to find $$$$
+  //   let eBilled = (workedMonths * eRate).toLocaleString('en-US', {
+  //     style: 'currency',
+  //     currency: 'USD'
+  //   });
 
-    //creating a new row in the table
-    let newRow = $('<tr>').append(
-        $('<td>').text(eName),
-        $('<td>').text(eRole),
-        $('<td>').text(eStart),
-        $('<td>').text(workedMonths),
-        $('<td>').text(`$${eRate}`),
-        $('<td>').text(`${eBilled}`)
-    );
+  //creating a new row in the table
+  let newRow = $('<tr>').append(
+    $('<td>').text(dtName),
+    $('<td>').text(dtDestination),
+    $('<td>').text(dtTime),
+    $('<td>').text(dtFreq)
+    // $('<td>').text(`$${eRate}`),
+    // $('<td>').text(`${eBilled}`)
+  );
 
-    $('#employee-table>tbody').append(newRow);
+  $('#employee-table>tbody').append(newRow);
 });
