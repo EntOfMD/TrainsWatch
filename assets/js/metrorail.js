2ba3285fef65a48c9947782deec91c8002c4ecad
Ashell Fernando <ashellz2010@gmail.com>
2019-03-27 19:31:51 -0400

    starting on the metrorail API

---
+++ assets/js/metrorail.js
@@ -0,0 +1,41 @@
+/* Station Codes
+
+https://docs.google.com/spreadsheets/d/13Kz-v3Yjn6ork9vXyl8KLSgzf7KYuGNP9d7HPMd-Kzc/pub?hl=en&single=true&gid=0&output=html
+ */
+
+const config = {
+  _APIKEY_: `b4139bd1-ea5b-4bc7-aa99-c45f480f5ddd`,
+  qURL: `https://dcmetrohero.com/api/v1/`
+};
+
+const apiTypes = {
+  trainURL: `metrorail/trains/`,
+  trainMetrics: `metrorail/metrics`,
+  trips: {
+    // metrorail/trips/{fromStationCode}/{toStationCode}
+    fromStation: undefined,
+    toStation: undefined,
+    url: `metrorail/trips/`
+  },
+  tweets: `metrorail/tweets/`,
+  trainReports: `metrorail/trains/tags/`,
+  indTrainReports: {
+    trainId: undefined,
+    url: 'metrorail/trains/' + trainId + '/tags/'
+  }
+};
+
+// Get train info
+$.ajax({
+  type: 'GET',
+  url: config.qURL + apiTypes.trainURL,
+  contentType: 'application/json',
+  xhrFields: {
+    withCredentials: false
+  },
+  headers: {
+    apiKey: config._APIKEY_
+  }
+}).then(res => {
+  console.log(res);
+});
