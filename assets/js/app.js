cd784f29235ebb7f3bb6c0ea4874fc883f88b4fe
Ashell Fernando <ashellz2010@gmail.com>
2019-03-28 21:51:17 -0400

    added and removed comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-  apiKey: 'AIzaSyBc8V8kvpScxILBHm1bxkY2BdY3gLnw3mQ',
-  authDomain: 'trainswatch-81413.firebaseapp.com',
-  databaseURL: 'https://trainswatch-81413.firebaseio.com',
-  projectId: 'trainswatch-81413',
-  storageBucket: 'trainswatch-81413.appspot.com',
-  messagingSenderId: '997350962243'
+    apiKey: 'AIzaSyBc8V8kvpScxILBHm1bxkY2BdY3gLnw3mQ',
+    authDomain: 'trainswatch-81413.firebaseapp.com',
+    databaseURL: 'https://trainswatch-81413.firebaseio.com',
+    projectId: 'trainswatch-81413',
+    storageBucket: 'trainswatch-81413.appspot.com',
+    messagingSenderId: '997350962243'
 };
 
 firebase.initializeApp(config);
@@ -13,112 +13,103 @@ firebase.initializeApp(config);
 var db = firebase.database();
 
 $('#add-train-btn').on('click', e => {
-  e.preventDefault();
+    e.preventDefault();
 
-  // Grab user input
-  let tName = $('#train-name-input')
-    .val()
-    .trim();
+    // Grab user input
+    let tName = $('#train-name-input')
+        .val()
+        .trim();
 
-  let tDes = $('#destination-input')
-    .val()
-    .trim();
+    let tDes = $('#destination-input')
+        .val()
+        .trim();
 
-  let tTime = $('#train-first-input')
-    .val()
-    .trim();
+    let tTime = $('#train-first-input')
+        .val()
+        .trim();
 
-  let tFreq = $('#train-frequency-input')
-    .val()
-    .trim();
+    let tFreq = $('#train-frequency-input')
+        .val()
+        .trim();
 
-  // conditional checking if there are data in form fields.
-  // Don't want empty datasets
-  if (tName.length === 0 || tDes.length === 0 || tFreq.length === 0) {
-    $('#error')
-      .html('Please fill out ALL the form fields!')
-      .show();
-    setTimeout(() => {
-      $('#error').hide();
-    }, 3000);
-  } else {
-    //Create temp obj
+    // conditional checking if there are data in form fields.
+    // Don't want empty datasets
+    if (tName.length === 0 || tDes.length === 0 || tFreq.length === 0) {
+        $('#error')
+            .html('Please fill out ALL the form fields!')
+            .show();
+        setTimeout(() => {
+            $('#error').hide();
+        }, 3000);
+    } else {
+        //Create temp obj
 
-    let newLine = {
-      name: tName,
-      destination: tDes,
-      startTime: tTime,
-      frequency: tFreq
-    };
+        let newLine = {
+            name: tName,
+            destination: tDes,
+            startTime: tTime,
+            frequency: tFreq
+        };
 
-    //uploads temp obj to firebase db
-    //!!!!!! IMPORTANT
-    db.ref().push(newLine);
+        //uploads temp obj to firebase db
+        //!!!!!! IMPORTANT
+        db.ref().push(newLine);
 
-    //console logs
-    console.log(
-      `Entry for ${newLine.name} with destination of ${
-        newLine.destination
-      } and with start time ${newLine.startTime} with frequent of ${
-        newLine.frequency
-      } HAS BEEN ADDED!`
-    );
-    // Clears text values
-    $('#train-name-input').val('');
-    $('#destination-input').val('');
-    $('#train-first-input').val('');
-    $('#train-frequency-input').val('');
-    return false;
-  }
+        //console logs
+        console.log(
+            `Entry for ${newLine.name} with destination of ${
+                newLine.destination
+            } starting at ${newLine.startTime} that comes every ${
+                newLine.frequency
+            } mins HAS BEEN ADDED!`
+        );
+        // Clears text values
+        $('#train-name-input').val('');
+        $('#destination-input').val('');
+        $('#train-first-input').val('');
+        $('#train-frequency-input').val('');
+        return false;
+    }
 });
 
 // 3. Create a way to retrieve train info from the trainswatch database.
 
 //gets entries on the DB
 db.ref().on('child_added', childSnap => {
-  console.log('THIS IS A dataSnapShot ' + childSnap.val());
+    //declaring again for when a 'child is added'
+    let dtName = childSnap.val().name,
+        dtDestination = childSnap.val().destination,
+        dtTime = childSnap.val().startTime,
+        dtFreq = childSnap.val().frequency;
 
-  //declaring again for when a 'child is added'
-  let dtName = childSnap.val().name,
-    dtDestination = childSnap.val().destination,
-    dtTime = childSnap.val().startTime,
-    dtFreq = childSnap.val().frequency;
+    let arrivalTime = dtTime.split(':');
+    let tTime = moment()
+        .hours(arrivalTime[0])
+        .minutes(arrivalTime[1]);
+    let maximumTime = moment.max(moment(), tTime);
+    var tMins, aTime;
 
-  //logging the values
-  console.log(`${dtName} ${dtDestination} ${dtTime} ${dtFreq} `);
+    if (maximumTime === tTime) {
+        aTime = tTime.format('hh:mm A');
+        tMins = tTime.diff(moment(), 'minutes');
+    } else {
+        let timeDiff = moment().diff(tTime, 'minutes');
+        let tMod = timeDiff % dtFreq;
+        tMins = dtFreq - tMod;
 
-  let arrivalTime = dtTime.split(':');
-  let tTime = moment()
-    .hours(arrivalTime[0])
-    .minutes(arrivalTime[1]);
-  let maximumTime = moment.max(moment(), tTime);
-  var tMins, aTime;
+        aTime = moment()
+            .add(tMins, 'm')
+            .format('hh:mm A');
+    }
 
-  if (maximumTime === tTime) {
-    aTime = tTime.format('hh:mm A');
-    tMins = tTime.diff(moment(), 'minutes');
-  } else {
-    let timeDiff = moment().diff(tTime, 'minutes');
-    let tMod = timeDiff % dtFreq;
-    tMins = dtFreq - tMod;
+    //creating a new row in the table
+    let newRow = $('<tr>').append(
+        $('<td>').text(dtName),
+        $('<td>').text(dtDestination),
+        $('<td>').text(dtFreq),
+        $('<td>').text(aTime),
+        $('<td>').text(tMins)
+    );
 
-    aTime = moment()
-      .add(tMins, 'm')
-      .format('hh:mm A');
-  }
-
-  console.log(`tMins : ${tMins}
-  aTime: ${aTime}
-  `);
-
-  //creating a new row in the table
-  let newRow = $('<tr>').append(
-    $('<td>').text(dtName),
-    $('<td>').text(dtDestination),
-    $('<td>').text(dtFreq),
-    $('<td>').text(aTime),
-    $('<td>').text(tMins)
-  );
-
-  $('#employee-table>tbody').append(newRow);
+    $('#employee-table>tbody').append(newRow);
 });
